ac1219a17d5aa5118d6cc732dedb3b447700a771
fami <2220002370@iau.edu.sa>
2024-07-06 21:49:35 +0300

    Create GussingNum.py

---
+++ Python/GussingNum.py
@@ -0,0 +1,3 @@
+#This is Simple Project for Generating a Random Number within Range 0-100 ,and Asking for Guessing the Random Number
+#The User Input is Integer Value Entered Continuslly with a Hint if the guessed Number Match/Lowwer/Higher
+print("\t--Welcome to the Guess the Number Game--\nEnter a Random Number\n>") 
